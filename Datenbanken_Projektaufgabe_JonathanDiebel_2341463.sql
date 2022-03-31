d1651f08609ca93b517afc1b7944797590ad34fd
Jonathan Diebel <jonathan.diebel@web.de>
2022-03-31 17:34:46 +0200

    added drop table if exists

--- Datenbanken_Projektaufgabe_JonathanDiebel_2341463.sql
+++ Datenbanken_Projektaufgabe_JonathanDiebel_2341463.sql
@@ -5,3 +5,22 @@
 -- Erstelldatum:     31.03.2022
 -- Letzte Änderung:  xx.04.2022
 
+
+/*
+ * Delte tables if they already exist
+ */
+DROP TABLE if EXISTS mitarbeiter;
+DROP TABLE if EXISTS lieferant;
+DROP TABLE if EXISTS koch;
+DROP TABLE if EXISTS lieferzone;
+DROP TABLE if EXISTS kunde;
+DROP TABLE if EXISTS bestellung;
+DROP TABLE if EXISTS artikel;
+DROP TABLE if EXISTS wein;
+DROP TABLE if EXISTS pizza;
+DROP TABLE if EXISTS zutat;
+DROP TABLE if EXISTS besteht_aus;
+DROP TABLE if EXISTS ist_belegt_mit;
+DROP TABLE if EXISTS telefonnummern_mitarbeiter;
+DROP TABLE if EXISTS telefonnummern_kunden;
+
