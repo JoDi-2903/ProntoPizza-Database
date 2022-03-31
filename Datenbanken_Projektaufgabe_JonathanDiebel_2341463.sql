5768bd4b01ece60043ea9a6683e82974c953be35
Jonathan Diebel <jonathan.diebel@web.de>
2022-03-31 18:03:40 +0200

    restructured code and added first table

--- Datenbanken_Projektaufgabe_JonathanDiebel_2341463.sql
+++ Datenbanken_Projektaufgabe_JonathanDiebel_2341463.sql
@@ -1,14 +1,18 @@
--- DHBW Stuttgart | Datenbanken 1 | Semester 4
--- ********************************************
--- Autor:            Jonathan Diebel
--- Matrikelnummer:   2341463
--- Erstelldatum:     31.03.2022
--- Letzte Änderung:  xx.04.2022
+/* 
+ * DHBW Stuttgart | Datenbanken 1 | Semester 4
+ * ********************************************
+ * Autor:            Jonathan Diebel
+ * Matrikelnummer:   2341463
+ * Erstelldatum:     31.03.2022
+ * Letzte Änderung:  00.04.2022
+ */
 
+----------------------------------------------------------------------------------
 
 /*
  * Delte tables if they already exist
  */
+
 DROP TABLE if EXISTS mitarbeiter;
 DROP TABLE if EXISTS lieferant;
 DROP TABLE if EXISTS koch;
@@ -24,3 +28,28 @@ DROP TABLE if EXISTS ist_belegt_mit;
 DROP TABLE if EXISTS telefonnummern_mitarbeiter;
 DROP TABLE if EXISTS telefonnummern_kunden;
 
+----------------------------------------------------------------------------------
+
+/*
+ * Set up the tables
+ */
+
+-- Create the table "mitarbeiter"
+CREATE TABLE IF NOT EXISTS mitarbeiter 
+ (
+   	Steuer-ID integer,
+    Vorname varchar,
+    Nachname varchar NOT NULL,
+    Strasse varchar,
+    Hausnummer int,
+    Postleitzahl int,
+    Ort varchar,
+    Email varchar,
+    Gehalt int,
+    IBAN char(22), -- Länge IBAN ermitteln
+    BIC char(11),
+    Vertretung_fuer int,
+
+	PRIMARY KEY (Steuer-ID),
+	CHECK (Gehalt >= 450  and Gehalt =< 3500) 
+);	
