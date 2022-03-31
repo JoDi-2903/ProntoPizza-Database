330708d35ab8596754202b4a9f79e7170df6b4d0
JoDi-2903 <88625959+JoDi-2903@users.noreply.github.com>
2022-03-31 12:43:10 +0200

    Add files via upload

---
+++ Datenbanken_Projektaufgabe_JonathanDiebel_2341463.sql
@@ -0,0 +1,7 @@
+-- DHBW Stuttgart | Datenbanken 1 | Semester 4
+-- ********************************************
+-- Autor:            Jonathan Diebel
+-- Matrikelnummer:   2341463
+-- Erstelldatum:     31.03.2022
+-- Letzte Änderung:  xx.04.2022
+
