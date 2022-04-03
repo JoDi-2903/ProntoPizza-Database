b5afaaad4149b538a15cd1e0096c9d6ed48b0c90
JoDi-2903 <jonathan.diebel@web.de>
2022-04-03 16:04:22 +0200

    added Checks to tables

--- Datenbanken_Projektaufgabe_JonathanDiebel_2341463.sql
+++ Datenbanken_Projektaufgabe_JonathanDiebel_2341463.sql
@@ -4,7 +4,7 @@
  * Autor:            Jonathan Diebel
  * Matrikelnummer:   2341463
  * Erstelldatum:     31.03.2022
- * Letzte Änderung:  00.04.2022
+ * Letzte Änderung:  03.04.2022
  */
 
 ----------------------------------------------------------------------------------
@@ -45,8 +45,8 @@ CREATE TABLE IF NOT EXISTS mitarbeiter
     Postleitzahl int,
     Ort varchar,
     Email varchar,
-    Gehalt int,
-    IBAN char(22),
+    Gehalt int NOT NULL,
+    IBAN char(22) NOT NULL,
     BIC char(11),
     Vertretung_fuer int,
 
@@ -65,14 +65,14 @@ CREATE TABLE IF NOT EXISTS lieferant
     Postleitzahl int,
     Ort varchar,
     Email varchar,
-    Gehalt int,
-    IBAN char(22),
+    Gehalt int NOT NULL,
+    IBAN char(22) NOT NULL,
     BIC char(11),
-    Fuehrerschein-ID char(11) NOT NULL, --Führerschein ID Länge prüfen
+    Fuehrerschein-ID char(11) NOT NULL,
     Vertretung_fuer int,
 
 	PRIMARY KEY (Steuer-ID),
-	CHECK (Gehalt >= 450  and Gehalt =< 3500) --Gehaltsbereiche anpassen
+	CHECK (Gehalt >= 450  and Gehalt =< 3500)
 );
 
 -- Create the table "koch"
@@ -86,24 +86,26 @@ CREATE TABLE IF NOT EXISTS koch
     Postleitzahl int,
     Ort varchar,
     Email varchar,
-    Gehalt int,
-    IBAN char(22),
+    Gehalt int NOT NULL,
+    IBAN char(22) NOT NULL,
     BIC char(11),
     Kuechenposten char(3) NOT NULL,
     Vertretung_fuer int,
 
 	PRIMARY KEY (Steuer-ID),
-	CHECK (Gehalt >= 450  and Gehalt =< 3500) --Gehaltsbereiche anpassen
+	CHECK (Gehalt >= 450  and Gehalt =< 4000),
+    CHECK (Kuechenposten IN ('PZA', 'VIN', 'ABW', 'AZU'))
 );
 
 -- Create the table "lieferzone"
 CREATE TABLE IF NOT EXISTS lieferzone
  (
     Zonen-Nummer int,
-    Bezeichnung varchar,
+    Bezeichnung char(13),
     Zustaendiger_Fahrer int,
 
-	PRIMARY KEY (Zonen-Nummer)
+	PRIMARY KEY (Zonen-Nummer),
+    CHECK (Bezeichnung IN ('Biberach', 'Boeckingen', 'Frankenbach', 'Horkheim', 'Kirchhausen', 'Klingenberg', 'Neckargartach', 'Sontheim'))
 );
 
 -- Create the table "kunde"
@@ -112,10 +114,10 @@ CREATE TABLE IF NOT EXISTS kunde
     Kunden-Nummer int,
     Vorname varchar,
     Nachname varchar NOT NULL,
-    Strasse varchar,
-    Hausnummer int,
-    Postleitzahl int,
-    Ort varchar,
+    Strasse varchar NOT NULL,
+    Hausnummer int NOT NULL,
+    Postleitzahl int NOT NULL,
+    Ort varchar NOT NULL,
     Email varchar,
 
 	PRIMARY KEY (Kunden-Nummer)
@@ -126,8 +128,8 @@ CREATE TABLE IF NOT EXISTS bestellung
  (
     Bestell-Nummer int,
     Zeitstempel datetime,
-    Artikelanzahl int,
-    Preis decimal,
+    Artikelanzahl int NOT NULL,
+    Preis decimal NOT NULL,
     zubereitet_von int,
     ausgeliefert_von int,
     erteilt_von int,
@@ -139,34 +141,39 @@ CREATE TABLE IF NOT EXISTS bestellung
 CREATE TABLE IF NOT EXISTS artikel
  (
     Artikel-Nummer int,
-    Kategorie varchar,
+    Kategorie char(9),
     Stueckpreis decimal,
     
-	PRIMARY KEY (Artikel-Nummer)
+	PRIMARY KEY (Artikel-Nummer),
+    CHECK (Kategorie IN ('Speisen', 'Getraenke'))
 );
 
 -- Create the table "wein"
 CREATE TABLE IF NOT EXISTS wein
  (
     Artikel-Nummer int,
-    Kategorie varchar,
+    Kategorie char(9),
     Stueckpreis decimal,
     Jahrgang int,
     Rebsorte varchar,
     Vorrat int,
     
-	PRIMARY KEY (Artikel-Nummer)
+	PRIMARY KEY (Artikel-Nummer),
+    CHECK (Kategorie IN ('Speisen', 'Getraenke')),
+    CHECK (Jahrgang >= 1990 and Jahrgang =< date_part('year', CURRENT_DATE))
 );
 
 -- Create the table "pizza"
 CREATE TABLE IF NOT EXISTS pizza
  (
     Artikel-Nummer int,
-    Kategorie varchar,
+    Kategorie char(9),
     Stueckpreis decimal,
     Groesse char(6),
     
-	PRIMARY KEY (Artikel-Nummer)
+	PRIMARY KEY (Artikel-Nummer),
+    CHECK (Kategorie IN ('Speisen', 'Getraenke')),
+    CHECK (Groesse IN ('small', 'medium', 'large', 'family', 'party'))
 );
 
 -- Create the table "zutat"
@@ -175,7 +182,7 @@ CREATE TABLE IF NOT EXISTS zutat
     Zutaten-Nummer int,
     Bezeichnung varchar,
     Hersteller varchar,
-    vegetarisch? boolean,
+    vegetarisch? boolean NOT NULL,
     Vorrat int,
     
 	PRIMARY KEY (Zutaten-Nummer)
@@ -204,9 +211,10 @@ CREATE TABLE IF NOT EXISTS Telefonnummern_Mitarbeiter
  (
     Telefonnummer varchar,
     Art char(8),
-    Besitzer_M int,
+    Besitzer_M int NOT NULL,
     
-	PRIMARY KEY (Telefonnummer)
+	PRIMARY KEY (Telefonnummer),
+    CHECK (Art IN ('festnetz', 'mobil', 'fax'))
 );
 
 -- Create the table "Telefonnummern_Kunden"
@@ -214,7 +222,8 @@ CREATE TABLE IF NOT EXISTS Telefonnummern_Kunden
  (
     Telefonnummer varchar,
     Art char(8),
-    Besitzer_K int,
+    Besitzer_K int NOT NULL,
     
-	PRIMARY KEY (Telefonnummer)
+	PRIMARY KEY (Telefonnummer),
+    CHECK (Art IN ('festnetz', 'mobil', 'fax'))
 );
