f68ff573932bc9a61c62ba40df0ca84a193e5b70
JoDi-2903 <jonathan.diebel@web.de>
2022-04-06 23:53:12 +0200

    resorted the table drop in the beginning

--- Datenbanken_Projektaufgabe_JonathanDiebel_2341463.sql
+++ Datenbanken_Projektaufgabe_JonathanDiebel_2341463.sql
@@ -13,20 +13,20 @@
  * Delete tables if they already exist
  */
 
-DROP TABLE if EXISTS mitarbeiter;
-DROP TABLE if EXISTS lieferant;
-DROP TABLE if EXISTS koch;
-DROP TABLE if EXISTS lieferzone;
-DROP TABLE if EXISTS kunde;
-DROP TABLE if EXISTS bestellung;
-DROP TABLE if EXISTS artikel;
-DROP TABLE if EXISTS wein;
-DROP TABLE if EXISTS pizza;
-DROP TABLE if EXISTS zutat;
-DROP TABLE if EXISTS besteht_aus;
-DROP TABLE if EXISTS ist_belegt_mit;
 DROP TABLE if EXISTS telefonnummern_mitarbeiter;
 DROP TABLE if EXISTS telefonnummern_kunden;
+DROP TABLE if EXISTS besteht_aus;
+DROP TABLE if EXISTS ist_belegt_mit;
+DROP TABLE if EXISTS lieferzone;
+DROP TABLE if EXISTS zutat;
+DROP TABLE if EXISTS wein;
+DROP TABLE if EXISTS pizza;
+DROP TABLE if EXISTS artikel;
+DROP TABLE if EXISTS bestellung;
+DROP TABLE if EXISTS kunde;
+DROP TABLE if EXISTS lieferant;
+DROP TABLE if EXISTS koch;
+DROP TABLE if EXISTS mitarbeiter;
 
 ----------------------------------------------------------------------------------
 
